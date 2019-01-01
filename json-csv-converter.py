e766fddb15432419fac8aaa90b265aa6db9757b7
Jonathan Hsu <jhsu98@gmail.com>
2019-01-01 05:29:57 +0000

    simplified getting column names from CSV

--- json-csv-converter.py
+++ json-csv-converter.py
@@ -26,10 +26,7 @@ except:
 else:
     # CONVERT CSV TO JSON
     if extension.lower() == "csv":
-        keys = []
-        for i in range(0,len(data[0])):
-            keys.append(data[0][i])
-
+        keys = data[0]
         converted = []
 
         for i in range(1, len(data)):
@@ -37,5 +34,3 @@ else:
             for j in range(0,len(data[i])):
                 obj[keys[j]] = data[i][j]
             converted.append(obj)
-        
-        print(converted)
